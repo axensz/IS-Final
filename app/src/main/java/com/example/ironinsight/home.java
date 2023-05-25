f54a4ad4393779c8223370ef1e9dceccd9ee65b4
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2023-05-25 01:22:22 -0500

    Cambios en home.java

--- app/src/main/java/com/example/ironinsight/home.java
+++ app/src/main/java/com/example/ironinsight/home.java
@@ -13,7 +13,7 @@ public class home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 
     public void regresar(View view) {
